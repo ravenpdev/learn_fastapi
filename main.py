afaa0d26fb47ebe1f85c80b4ef192f38c4d2ff36
RavenP <ravenp.dev@gmail.com>
2023-06-23 04:49:32 +0800

    Query Parameters

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from enum import Enum
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
+
+fake_posts_db = [
+    {"id": 1, "title": "Post 1"},
+    {"id": 2, "title": "Post 2"},
+    {"id": 3, "title": "Post 3"},
+]
 
 
 @app.get("/")
@@ -10,13 +16,55 @@ def root():
 
 
 @app.get("/posts")
-def get_posts():
-    return {"data": "this is your posts"}
+def get_posts(skip: int = 0, limit: int = 10):
+    return fake_posts_db[skip : skip + limit]
 
 
 @app.get("/posts/{id}")
-def read_post(id: int):
-    return {"post_id": id}
+def read_post(id: int, q: str | None = None, short: bool = False):
+    post: dict[str, int | str] = {"post_id": id}
+
+    if q:
+        post.update({"q": q})
+    if not short:
+        post.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return post
+
+
+@app.post("/posts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"data": {"id": 1, "title": "post 1"}}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int,
+    item_id: str,
+    q: str | None = None,
+    short: bool = False,
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+
+    return item
+
+
+# needy, a required str
+# skip, an int with a default value of 0.
+# limit, an optional int.
+@app.get("/items/{item_id}")
+def read_item(item_id: int, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
 
 class ModelName(str, Enum):
