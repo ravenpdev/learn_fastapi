55af3bb8f2513c844265b88a9a4c24ba1dd6dee9
RavenP <ravenp.dev@gmail.com>
2023-06-23 05:29:38 +0800

    Request Body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from pydantic import BaseModel
 from fastapi import Body, FastAPI
 
 app = FastAPI()
@@ -39,6 +40,39 @@ def create_post(payload: dict = Body(...)):
     return {"data": {"id": 1, "title": "post 1"}}
 
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items")
+def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price * item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+# needy, a required str
+# skip, an int with a default value of 0.
+# limit, an optional int.
+@app.get("/items/{item_id}")
+def read_item(item_id: int, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
 @app.get("/users/{user_id}/items/{item_id}")
 async def read_user_item(
     user_id: int,
@@ -55,15 +89,6 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
 
-    return item
-
-
-# needy, a required str
-# skip, an int with a default value of 0.
-# limit, an optional int.
-@app.get("/items/{item_id}")
-def read_item(item_id: int, needy: str, skip: int = 0, limit: int | None = None):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
 
 
