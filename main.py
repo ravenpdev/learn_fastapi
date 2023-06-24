c10dbd34b49caab9788382a7b082ae3307c8c655
RavenP <ravenp.dev@gmail.com>
2023-06-25 05:48:42 +0800

    Feature: Extra Models

--- main.py
+++ main.py
@@ -1,163 +1,98 @@
-from enum import Enum
-from typing import Annotated
-from pydantic import BaseModel
-from fastapi import FastAPI, Path, Query
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
-class Post(BaseModel):
-    title: str
-    content: str | None = None
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
 
 
-fake_posts_db = [
-    {"id": 1, "title": "Post 1"},
-    {"id": 2, "title": "Post 2"},
-    {"id": 3, "title": "Post 3"},
-]
+class UserIn(UserBase):
+    password: str
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+class UserOut(UserBase):
+    pass
 
 
-@app.get("/posts")
-def get_posts(skip: int = 0, limit: int = 10):
-    return fake_posts_db[skip : skip + limit]
+class UserInDB(UserBase):
+    hashed_password: str
 
 
-@app.get("/posts/{id}")
-def read_post(id: int, q: str | None = None, short: bool = False):
-    post: dict[str, int | str] = {"post_id": id}
-
-    if q:
-        post.update({"q": q})
-    if not short:
-        post.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return post
-
-
-@app.post("/posts")
-def create_post(post: Post):
-    print(post)
-    return {"data": post}
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+# class UserIn(BaseModel):
+#     username: str
+#     password: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+#
+# class UserOut(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+#
+# class UserInDB(BaseModel):
+#     username: str
+#     hashed_password: str
+#     email: EmailStr
+#     full_name: str | None = None
 
 
-@app.get("/items")
-def all_items(
-    q: Annotated[
-        str | None,
-        Query(
-            alias="item-query",
-            min_length=5,
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            deprecated=True,
-            include_in_schema=False,
-        ),
-    ] = None
-):
-    # def all_items(
-    #     q: Annotated[
-    #         list[str] | None,
-    #         Query(
-    #             title="Query String",
-    #             description="Query string for the items to search in the database that have a good match",
-    #         ),
-    #     ] = None
-    # ):
-    results: dict[str, list[dict[str, str]] | list[str] | str] = {
-        "items": [{"item_id": "Foo"}, {"item_id": "bar"}]
-    }
-    if q:
-        results.update({"q": q})
-    return results
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
 
 
-@app.post("/items")
-def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price * item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
 
 
-# needy, a required str
-# skip, an int with a default value of 0.
-# limit, an optional int.
-@app.get("/items/{item_id}")
-def read_item(
-    item_id: Annotated[int, Path(title="The ID of the item to get")],
-    q2: Annotated[int, Query(ge=1, le=10)],
-    q: Annotated[str | None, Query(alias="item-query")] = None,
-):
-    results: dict[str, str | int] = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    return results
+@app.post("/user", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
 
 
-# def read_item(item_id: int, needy: str, skip: int = 0, limit: int | None = None):
-#     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-#     return item
+# Union or anyOf
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
+class BaseItem(BaseModel):
+    description: str
+    type: str
 
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int,
-    item_id: str,
-    q: str | None = None,
-    short: bool = False,
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-
-    return item
+class CarItem(BaseItem):
+    type = "car"
 
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+class PlaneItem(BaseItem):
+    type = "plane"
+    size: int
 
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+items = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
 
-    if model_name is ModelName.lenet:
-        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+    return items[item_id]
 
 
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/items", response_model=list[BaseItem])
+async def read_items():
+    return items
